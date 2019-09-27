f94422366bd7972d6f16737a52ee54d028645a21
aekkarat5910110412 <32357023+aekkarat5910110412@users.noreply.github.com>
2019-09-27 20:55:22 +0700

    click

--- WordCard.js
+++ WordCard.js
@@ -5,35 +5,46 @@ Component {
 import _ from 'lodash';
 const prepareStateFromWord = (given_word) => {
     let word = given_word.toUpperCase()
-    let chars = _.shuffle(Array.from(word))
-    return {
-        word,
-        chars,
-        attempt: 1,
-        guess: [],
-        completed: false
-        }
-   }
-   export default class WordCard extends Component {
-    constructor(props){
-        super(props)
-        this.state = prepareStateFromWord(this.props.value)
-    }
-
-   activationHandler = (c) => {
-    let guess = [...this.state.guess, c]
-    this.setState({guess})
-    if(guess.length == this.state.chars.length){
-        if(guess.join('').toString() == this.state.word){
+@@ -28,23 +26,41 @@ const prepareStateFromWord = (given_word) => {
     this.setState({guess: [], completed: true})
         }else{
     this.setState({guess: [], attempt: this.state.attempt + 1})
+    activationHandler = (c) => {
+        let guess = [this.state.guess]+c
+        this.setState({guess})
+        if(guess.length === this.state.chars.length){
+            if(guess === this.state.word){
+                this.setState({guess: [], completed: true})
+            }else{
+                this.setState({guess: [], attempt: this.state.attempt + 1})
+            }
         }
     }
    }
 
 
 render() {
+    render(){	    
+        return(	       
+
+            <div className="App">	
+            <h3>จงเรียงประโยคต่อไปนี้ให้ถูกต้อง</h3>
+                {  
+                Array.from(this.state.chars).map((c, i) => <CharacterCard value = {c} key = {i} 
+                attempt={this.state.attempt}
+                activationHandler = {this.activationHandler} />
+                )
+                }
+               <p>Round : {this.state.attempt}</p>
+               <p>Hint : The first program in C </p>
+                <h1><font color="green">{this.state.completed? "You Win !!" : ""}</font></h1>
+                </div>	           
+            );	       
+
+    } 
+}
+
+
 
 return (
     <div>
